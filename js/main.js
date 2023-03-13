9b0698d6cb9477d6dd6989ee09e464a7ed061e3b
fadeonsp <edfadeon@gmail.com>
2023-03-13 01:20:11 -0300

    criando acões no js

--- js/main.js
+++ js/main.js
@@ -30,3 +30,48 @@ function scrollHeader() {
     if(this.scrollY >= 50) header.classList.add('scroll-header');
     else header.classList.remove('scroll-header')
 }
+
+/*----------exibir scroll up------------*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scrollUp')
+
+/*----------ativando sections link------------*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active-link')
+        }else {
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active-link')
+        }
+    })
+}
+
+/*----------scroll animation------------*/
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2000,
+    delay: 400,
+   // reset: true
+})
+sr.reveal(`.home__header, .section__title`, {delay: 600})
+sr.reveal(`.home__footer`, {delay: 700})
+sr.reveal(`.home__img`, {delay: 900, origin: 'top'})
+
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, footer__copy`, {origin: 'top', interval: 100})
+sr.reveal(`.specs__data, .discount_animate` , {origin: 'left', interval: 100})
+sr.reveal(`.specs__img, .discount__img`, {origin: 'right'})
+sr.reveal(`.case_img`, {origin: 'top'})
+sr.reveal(`.case_data`)
+
