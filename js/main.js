233b465c2674b6848915694640037bbc5181d87f
fadeonsp <edfadeon@gmail.com>
2023-03-13 00:27:39 -0300

    criando as sections

--- js/main.js
+++ js/main.js
@@ -23,3 +23,10 @@ const navLink = document.querySelectorAll('.nav__link')
         navMenu.classList.remove('show-menu')
    }
    navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*-----------mudando cor header------*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
